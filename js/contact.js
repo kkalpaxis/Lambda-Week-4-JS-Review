1ae67faa21f9f73d5af666c000b8264266ef16ee
Karina Kalpaxis <kalpaxis1@gmail.com>
2019-07-10 16:57:32 -0400

    WIP: Added Event Listeners to Contact Page

--- js/contact.js
+++ js/contact.js
@@ -3,17 +3,35 @@ nameColor.addEventListener('mouseover', () => {
     nameColor.style.color = '#8aa1b8';
 });
 
-document.getElementById("name").addEventListener('click', nameExample());
-function nameExample() {
-    var nameEx = document.getElementById("name").value;
-    nameEx = "FirstName LastName";
+function nameExample(event) {
+    document.getElementById("name").value = "FirstName LastName";
+}
+
+function emailExample(event) {
+    document.getElementById("email").value = "email@address.com";
 } 
 
-document.getElementById("email").addEventListener('click', emailExample());
-function emailExample() {
-    var emailEx = document.getElementById("email").value;
-    emailEx = "email@address.com";
-} 
+function nameClear (event) {
+    document.getElementById("name").value = "";
+}
+
+function emailClear (event) {
+    document.getElementById("email").value = "";
+}
+
+const formName = document.getElementById("name");
+formName.addEventListener('mouseover', nameExample);
+formName.addEventListener('click', nameClear);
+
+const formEmail = document.getElementById("email");
+formEmail.addEventListener('mouseover', emailExample);
+formEmail.addEventListener('click', emailClear);
+
+const formSubmit = document.getElementById("submit");
+formSubmit.addEventListener('click', () => {
+    alert('Thank you!');
+});
+
 
 
 
